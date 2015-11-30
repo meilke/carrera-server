9a096755af78db6e03c6849b3dca101a48eeb1aa
Christian Meilke <christian.meilke@googlemail.com>
2015-11-30 10:25:23 +0000

    Multi player setup

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var _ = require('lodash');
 
 var config = {
-  ledPin: { bcm: 17, wpi: 0 },
-  photoPin: { bcm: 4, wpi: 7 },
-  players: [ { name: 'first', color: 'red' } ]
+  players: [ { name: 'first', color: 'red', photoPin: { bcm: 4, wpi: 7 }, ledPin: { bcm: 17, wpi: 0 } } ]
 };
 
 var gpio = require('./lib/gpio')(config);
 var race = require('./lib/race')(config);
+race.start();
 
 app.get('/', function(req, res) {
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+function findPlayerByName(playerName) {
+  var matchingPlayers = _.filter(race.getPlayers(), function (player) { return player.name === playerName; });
+  return matchingPlayers[0];
+}
+
+gpio.watch(function(player) {
+  var racePlayer = findPlayerByName(player.name);
+  race.addLap(racePlayer);
+  io.emit('lap', race.getPlayers());
 });
 
 io.on('connection', function(socket) {
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
-  });
-  gpio.watchForSwitch(function() {
-    console.log('lap');
-    io.emit('lap', 'lap');
   });
 });
 
